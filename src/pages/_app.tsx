3e7cd7133d74f4c3e409983084c2c7e1acd71be3
Yafeth Moya <lymoya@unah.hn>
2023-05-21 15:07:51 -0600

    Add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { type AppType } from "next/app";
-
+import { ClerkProvider, SignIn } from '@clerk/nextjs'
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+
+  return (
+    <ClerkProvider>
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
+
 };
 
 export default api.withTRPC(MyApp);
